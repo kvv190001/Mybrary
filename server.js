fd12acb7ae2d337e638f04022dc44bc8adf30b2e
Viet Khue Vu <kvv190001@users.noreply.github.com>
2023-05-26 15:25:39 -0500

    Finish Project

--- server.js
+++ server.js
@@ -6,15 +6,18 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 app.set('view engine', 'ejs')
 // Nodejs automatically look for ejs files in views folder
 // app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit : '10mb', extended: false}))
 
@@ -27,5 +30,6 @@ db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 app.listen(process.env.PORT || 3000)
